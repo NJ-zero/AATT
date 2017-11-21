21ed09dc156a3a9b590b989e8f2bfe3fe479b9a3
NJ-zero <821854810@qq.com>
2017-11-21 19:30:30 +0800

    页面优化，增加图标，修改单位

--- aatt_pyqt.py
+++ aatt_pyqt.py
@@ -11,7 +11,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(1114, 299)
+        Form.resize(1114, 701)
         self.all = QtWidgets.QTextEdit(Form)
         self.all.setGeometry(QtCore.QRect(957, 97, 147, 192))
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
@@ -222,6 +222,17 @@ class Ui_Form(object):
         sizePolicy.setHeightForWidth(self.cleartext.sizePolicy().hasHeightForWidth())
         self.cleartext.setSizePolicy(sizePolicy)
         self.cleartext.setObjectName("cleartext")
+        self.mem_plot = MatplotlibWidget(Form)
+        self.mem_plot.setGeometry(QtCore.QRect(70, 300, 1021, 381))
+        self.mem_plot.setObjectName("mem_plot")
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(0, 410, 61, 23))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setObjectName("pushButton")
 
         self.retranslateUi(Form)
         self.close.clicked.connect(Form.close)
@@ -232,7 +243,7 @@ class Ui_Form(object):
         Form.setWindowTitle(_translate("Form", "Form"))
         self.label_9.setText(_translate("Form", "上传流量(k)"))
         self.label_10.setText(_translate("Form", "总流量(k)"))
-        self.label_6.setText(_translate("Form", "mem占用(k)"))
+        self.label_6.setText(_translate("Form", "mem占用(M)"))
         self.label_7.setText(_translate("Form", "cpu占用(%)"))
         self.label_8.setText(_translate("Form", "下载流量(k)"))
         self.checkdev.setText(_translate("Form", "检查设备"))
@@ -253,4 +264,6 @@ class Ui_Form(object):
         self.end.setText(_translate("Form", "结束"))
         self.close.setText(_translate("Form", "关闭"))
         self.cleartext.setText(_translate("Form", "清除"))
+        self.pushButton.setText(_translate("Form", "图表"))
 
+from MatplotlibWidget import MatplotlibWidget
